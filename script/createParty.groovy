3c9db93bee4f4c0e6952ee28a5afbca96c57e8db
Kumar Kartikey Choudhary <choudharykartik733@gmail.com.com>
2026-01-21 18:24:24 +0530

    adding groovy files

--- script/createParty.groovy
+++ script/createParty.groovy
@@ -3,11 +3,9 @@ import org.moqui.entity.EntityValue
 def ec = context.ec
 
 EntityValue party = ec.entity.makeValue("com.party.Party")
-
 if (!party.partyId) {
     party.setSequencedIdPrimary()
 }
 party.setFields(context, true, null, null)
 party.create()
-
 return [partyId : party.partyId]
